d982e0e323e209f19dc31b9fae3ccb618b11fe49
xifOO <wedyi28111@gmail.com>
2025-11-19 15:53:40 +0300

    feat: create scopes and entities

--- src/entity.py
+++ src/entity.py
@@ -1,36 +1,95 @@
-from dataclasses import dataclass
 from typing import Any, List, Optional
 
 
-@dataclass(frozen=True)
-class ArgumentEntity:
-    name: str
-    annotation: Optional[Any]
-    default: Optional[Any]
-    is_keyword_only: bool = False
-    is_positional_only: bool = False
-
-
-@dataclass
 class CodeEntity:
-    name: str
-    line: int
+    def __init__(self, node_id: int, name: str, line: int) -> None:
+        self.node_id = node_id
+        self.name = name
+        self.line = line
 
 
-@dataclass
+class ArgumentEntity(CodeEntity):
+    __slots__ = (
+        "name",
+        "node_id",
+        "annotation",
+        "default",
+        "is_keyword_only",
+        "is_positional_only",
+    )
+
+    def __init__(
+        self,
+        node_id: int,
+        name: str,
+        line: int,
+        annotation: Optional[Any],
+        default: Optional[Any],
+        is_keyword_only: bool = False,
+        is_positional_only: bool = False,
+    ) -> None:
+        self.annotation = annotation
+        self.default = default
+        self.is_keyword_only = is_keyword_only
+        self.is_positional_only = is_positional_only
+        super().__init__(node_id, name, line)
+
+
 class FunctionEntity(CodeEntity):
-    args: List[ArgumentEntity]
-    decorators: List[str]
-    type_params: List[str]
-    returns: Optional[Any] = None
+    def __init__(
+        self,
+        node_id: int,
+        name: str,
+        line: int,
+        args: List[ArgumentEntity],
+        decorators: List[str],
+        type_params: List[str],
+        returns: Optional[Any] = None,
+        is_method: bool = False,
+    ) -> None:
+        self.args = args
+        self.decorators = decorators
+        self.type_params = type_params
+        self.returns = returns
+        self.is_method = is_method
+        super().__init__(node_id, name, line)
 
-    is_method: bool = False
 
-
-@dataclass
 class ClassEntity(CodeEntity):
-    bases: List[str]
-    methods: List[FunctionEntity]
-    decorators: List[str]
-    keywords: List[str]
-    type_params: List[str]
+    def __init__(
+        self,
+        node_id: int,
+        name: str,
+        line: int,
+        bases: List[str],
+        methods: List[FunctionEntity],
+        decorators: List[str],
+        keywords: List[str],
+        type_params: List[str],
+    ) -> None:
+        self.bases = bases
+        self.methods = methods
+        self.decorators = decorators
+        self.keywords = keywords
+        self.type_params = type_params
+        super().__init__(node_id, name, line)
+
+
+class ModuleEntity(CodeEntity):
+    def __init__(self, node_id: int, name: str, line: int) -> None:
+        super().__init__(node_id, name, line)
+
+    def get_classes(self) -> List[ClassEntity]: ...
+
+    def get_functions(self) -> List[FunctionEntity]: ...
+
+    def find_node_by_name(self, name: str) -> Optional[CodeEntity]: ...
+
+
+class ImportEntity(CodeEntity):
+    def __init__(
+        self, node_id: int, name: str, line: int, module_name: str, alias: Optional[str]
+    ) -> None:
+        self.module_name = module_name
+        self.alias = alias
+        super().__init__(node_id, name, line)
