d4b02f741074a40afcf251e07b03023de61e2d15
xifOO <wedyi28111@gmail.com>
2025-12-17 01:42:07 +0300

    refactor: remove unused rebind tracking

--- src/analysis/side_effect.py
+++ src/analysis/side_effect.py
@@ -1,15 +1,22 @@
 import ast
-from typing import Dict, NamedTuple, Set
+from typing import Dict, List, NamedTuple, Set, TypeAlias
 
 from entity import CallEntity, FunctionEntity, GlobalDeclEntity, NonlocalDeclEntity, VariableEntity
+from index.call import CALL_MAP
 from index.scopes import CHILDREN_MAP, NODE_ID_MAP
+
+
+ArgName: TypeAlias = str
+MethodName: TypeAlias = str
+
+ObjectName: TypeAlias = str
+AttrName: TypeAlias = str
 
 
 class SideEffect(NamedTuple):
     reads: frozenset[str]
     writes: frozenset[str]
     mutates: frozenset[tuple[str, str]]
-    rebinds: frozenset[str]
 
 
 MUTATING_METHODS = {
@@ -20,77 +27,29 @@ MUTATING_METHODS = {
 SIDE_EFFECT_MAP: Dict[int, SideEffect] = {}
 
 
-def _collect_rebinds(func: FunctionEntity, global_names, nonlocal_names):
-    rebinds: Set[str] = set()
-
-    for node in ast.walk(func.ast_node):
-        if isinstance(node, ast.Assign):
-            for t in node.targets:
-                if isinstance(t, ast.Name):
-                    if t.id not in global_names and t.id not in nonlocal_names:
-                        rebinds.add(t.id)
-        
-        elif isinstance(node, ast.AnnAssign):
-            if isinstance(node.target, ast.Name):
-                if node.target.id not in global_names and node.target.id not in nonlocal_names:
-                    rebinds.add(node.target.id)
-
-        elif isinstance(node, ast.AugAssign):
-            if isinstance(node.target, ast.Name):
-                if node.target.id not in global_names and node.target.id not in nonlocal_names:
-                    rebinds.add(node.target.id)
-
-        elif isinstance(node, ast.For):
-            if isinstance(node.target, ast.Name):
-                if node.target.id not in global_names and node.target.id not in nonlocal_names:
-                    rebinds.add(node.target.id)
-
-        elif isinstance(node, ast.withitem):
-            if isinstance(node.optional_vars, ast.Name):
-                if node.optional_vars.id not in global_names and node.optional_vars.id not in nonlocal_names:
-                    rebinds.add(node.optional_vars.id)
-
-        elif isinstance(node, ast.ExceptHandler):
-            if node.name:
-                if node.name not in global_names and node.name not in nonlocal_names:
-                    rebinds.add(node.name)
-
-    return rebinds
-
 def analyze_function(func: FunctionEntity):
-    local_names = {arg.name for arg in func.args}
-
     child_ids = CHILDREN_MAP.get(func.node_id, [])
 
     global_names: Set[str] = set()
     nonlocal_names: Set[str] = set()
-    variable_entities = []
-
-    local_bindings: Set[str] = {arg.name for arg in func.args}
+    local_names: Set[str] = {arg.name for arg in func.args}
+    variable_entities: List[VariableEntity] = []
 
     writes: Set[str] = set()
-    mutated_arguments: Set[tuple[str, str]] = set()
-    mutated_attributes: Set[tuple[str, str]] = set()
+    mutated_arguments: Set[tuple[ArgName, MethodName]] = set()
+    mutated_attributes: Set[tuple[ObjectName, AttrName]] = set()
     reads: Set[str] = set()
 
     for child_id in child_ids:
-        ent = NODE_ID_MAP.get(child_id)
+        ent = NODE_ID_MAP.get(child_id) or CALL_MAP.get(child_id)
+        
         if isinstance(ent, VariableEntity):
             variable_entities.append(ent)
-            if isinstance(ent.value, ast.Call):
-                if isinstance(ent.value.func, ast.Attribute):
-                    attr = ent.value.func.attr
-                    if attr in MUTATING_METHODS:
-                        if isinstance(ent.value.func.value, ast.Name):
-                            obj_name = ent.value.func.value.id
-                            if obj_name in {arg.name for arg in func.args}:
-                                mutated_arguments.add((obj_name, attr))
-
+                
         elif isinstance(ent, CallEntity):
             arg_names = {arg.name for arg in func.args}
             if ent.is_method_call and ent.receiver in arg_names:
-                if ent.name in MUTATING_METHODS:
-                    mutated_arguments.add(ent.receiver)
+                mutated_arguments.add((ent.receiver, ent.name))
 
         elif isinstance(ent, GlobalDeclEntity):
             global_names.update(ent.names)
@@ -112,12 +71,9 @@ def analyze_function(func: FunctionEntity):
 
         if isinstance(target, ast.Attribute):
             if isinstance(target.value, ast.Name):
-                mutated_attributes.add((target.value.id, target.attr))
-
-    for child in ast.walk(func.ast_node):
-        if isinstance(child, ast.Name) and isinstance(child.ctx, ast.Store):
-            if child.id not in global_names and child.id not in nonlocal_names:
-                local_bindings.add(child.id)
+                attr = target.value.id
+                if attr in global_names or attr in nonlocal_names or attr in {arg.name for arg in func.args}:
+                    mutated_attributes.add((attr, target.attr))
 
     for node in ast.walk(func.ast_node):
         if isinstance(node, ast.Name) and isinstance(node.ctx, ast.Load) and node.id in global_names:
@@ -126,8 +82,7 @@ def analyze_function(func: FunctionEntity):
     info = SideEffect(
         reads=frozenset(reads),
         writes=frozenset(writes),
-        mutates=frozenset(mutated_arguments.union(mutated_attributes)),
-        rebinds=frozenset(_collect_rebinds(func, global_names, nonlocal_names))
+        mutates=frozenset(mutated_arguments.union(mutated_attributes))
     )
 
     return info
