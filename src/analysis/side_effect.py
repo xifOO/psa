d7b0d521536ec0ae4380301d038427b60e807b4d
xifOO <wedyi28111@gmail.com>
2025-12-19 01:44:52 +0300

    refactor: split mutates into arg and attr mutations

--- src/analysis/side_effect.py
+++ src/analysis/side_effect.py
@@ -16,23 +16,24 @@ AttrName: TypeAlias = str
 class SideEffect(NamedTuple):
     reads: frozenset[str]
     writes: frozenset[str]
-    mutates: frozenset[tuple[str, str]]
+
+    arg_mutates: frozenset[tuple[ArgName, MethodName]]
+    attr_mutates: frozenset[tuple[ObjectName, AttrName]]
 
 
 MUTATING_METHODS = {
     "append", "extend", "insert", "remove", "pop",
     "update", "clear", "add", "discard"
-}
+} # later append more methods
 
 SIDE_EFFECT_MAP: Dict[int, SideEffect] = {}
 
 
-def analyze_function(func: FunctionEntity):
+def analyze_side_effect(func: FunctionEntity) -> SideEffect: # add: aliases, rebinds
     child_ids = CHILDREN_MAP.get(func.node_id, [])
 
     global_names: Set[str] = set()
     nonlocal_names: Set[str] = set()
-    local_names: Set[str] = {arg.name for arg in func.args}
     variable_entities: List[VariableEntity] = []
 
     writes: Set[str] = set()
@@ -48,7 +49,7 @@ def analyze_function(func: FunctionEntity):
                 
         elif isinstance(ent, CallEntity):
             arg_names = {arg.name for arg in func.args}
-            if ent.is_method_call and ent.receiver in arg_names:
+            if ent.is_method_call and ent.receiver in arg_names and ent.name in MUTATING_METHODS:
                 mutated_arguments.add((ent.receiver, ent.name))
 
         elif isinstance(ent, GlobalDeclEntity):
@@ -75,6 +76,13 @@ def analyze_function(func: FunctionEntity):
                 if attr in global_names or attr in nonlocal_names or attr in {arg.name for arg in func.args}:
                     mutated_attributes.add((attr, target.attr))
 
+        elif isinstance(target, ast.Subscript):
+            value = target.value
+            if isinstance(value, ast.Name):
+                attr = value.id
+                if attr in global_names or attr in nonlocal_names or attr in {arg.name for arg in func.args}:
+                    mutated_attributes.add((attr, "__setitem__"))
+        
     for node in ast.walk(func.ast_node):
         if isinstance(node, ast.Name) and isinstance(node.ctx, ast.Load) and node.id in global_names:
             reads.add(node.id)
@@ -82,7 +90,8 @@ def analyze_function(func: FunctionEntity):
     info = SideEffect(
         reads=frozenset(reads),
         writes=frozenset(writes),
-        mutates=frozenset(mutated_arguments.union(mutated_attributes))
+        arg_mutates=frozenset(mutated_arguments),
+        attr_mutates=frozenset(mutated_attributes),
     )
 
     return info
