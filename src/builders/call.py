4749c54e46a9e9ce4ce45cc51296a1781a8ec490
xifOO <wedyi28111@gmail.com>
2026-01-06 00:57:29 +0300

    feat: add call builder for ast analysis

---
+++ src/builders/call.py
@@ -0,0 +1,59 @@
+import ast
+from entity import CallEntity
+from index.maps import Index
+from index.scopes import Scope
+from utils import _convert_call_args, _convert_keywords
+
+
+class CallBuilder:
+    def __init__(self, index: Index) -> None:
+        self.index = index
+
+    def run(self, scope: Scope):
+        self._collect_calls(scope)
+
+        for child_id in self.index.children_map.get(scope.node_id) or []:
+            child_scope = self.index.scope_map.get(child_id)
+            if child_scope:
+                self.run(child_scope)
+
+    def _collect_calls(self, scope: Scope):
+        for entity in scope.get_values():
+            node = entity.ast_node
+
+            if node is None:
+                continue
+
+            current_scope = scope
+
+            for child in ast.walk(node):
+                if isinstance(child, ast.Call):
+                    func_name = ""
+                    receiver = None
+                    is_method_call = False
+
+                    if isinstance(child.func, ast.Name):
+                        func_name = child.func.id
+                    elif isinstance(child.func, ast.Attribute):
+                        if isinstance(child.func.value, ast.Name):
+                            receiver_entity = current_scope.lookup(child.func.value.id)
+                            receiver = receiver_entity.name if receiver_entity else None
+                            func_name = child.func.attr
+                            is_method_call = True
+
+                    call_entity = CallEntity(
+                        node_id=self.index.next_node_id(),
+                        name=func_name,
+                        line=child.lineno,
+                        ast_node=child,
+                        args=_convert_call_args(child.args),
+                        keywords=_convert_keywords(child.keywords),
+                        is_method_call=is_method_call,
+                        receiver=receiver,
+                        scope=current_scope,
+                    )
+
+                    self.index.call_map.add(call_entity)
+                    self.index.children_map.link(
+                        current_scope.node_id, call_entity.node_id
+                    )
