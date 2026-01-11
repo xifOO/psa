aa563a6041a4ac68595618583b1bc03da0cef2ee
xifOO <wedyi28111@gmail.com>
2026-01-12 01:22:22 +0300

    refactor: restructure analyzer registry and retrieval

--- src/metrics/base.py
+++ src/metrics/base.py
@@ -1,21 +1,27 @@
 from abc import ABC, abstractmethod
-from typing import Any, List, Type
+from typing import Any, Iterator, Type
 
-_ANALYZER_REGISTRY: set[Type["Analyzer"]] = set()
+from entity import CodeEntity
+from index.maps import Index
 
 
 class Analyzer(ABC):
+    _ANALYZER_REGISTRY: set[Type["Analyzer"]] = set()
+
     def __init_subclass__(cls, **kwargs) -> None:
         """Register analyzer to registry."""
         super().__init_subclass__(**kwargs)
         if not cls.__name__.startswith("_"):
-            _ANALYZER_REGISTRY.add(cls)
+            cls._ANALYZER_REGISTRY.add(cls)
 
     @abstractmethod
     def applies_to(self, entity: Any) -> bool: ...
     @abstractmethod
-    def analyze(self, entity: Any) -> Any: ...
+    def analyze(self, index: Index, entity: Any) -> Any: ...
 
-
-def get_registered_analyzers() -> List[Type[Analyzer]]:
-    return list(_ANALYZER_REGISTRY)
+    @classmethod
+    def for_entity(cls, entity: CodeEntity) -> Iterator["Analyzer"]:
+        for analyzer in cls._ANALYZER_REGISTRY:
+            analyzer = analyzer()
+            if analyzer.applies_to(entity):
+                yield analyzer
