e2af62bfe9ac5bae6bf131ab0a4d37b7e1c86c8c
xifOO <wedyi28111@gmail.com>
2026-01-12 01:21:52 +0300

    feat: add main application runner setup

---
+++ src/main.py
@@ -0,0 +1,9 @@
+from pathlib import Path
+from config.rules import Config
+from runner import Runner
+
+
+config = Config.from_yaml("settings.yaml")
+
+runner = Runner(config, Path("./src"), [])
+print(runner.run())
